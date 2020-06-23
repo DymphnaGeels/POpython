0ddfa6ebc95ac125639df2266b3707da125373d0
Dymphna Geels <110533@st-maartenscollege.nl>
2020-06-23 14:13:12 +0000

    woordenlijst toegevoegd

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ print(b)
 lijst = ["-maximaal vijf keer raden","-een letter per beurt"]
 for item in lijst:
  print(item)
+ 
+woorden = [“informatica”, “informatiekunde”, “spelletje”, “aardigheidje”, “scholier”, “fotografie”, “waardebepaling”, “specialiteit”, “verzekering”, “universiteit”, “heesterperk”]
+
+# woord_1 = random.choice(woorden)
+
+
+ 
