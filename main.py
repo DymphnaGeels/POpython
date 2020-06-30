39b4547db5e8f936a93d2c9530c1123163397351
Dymphna Geels <110533@st-maartenscollege.nl>
2020-06-30 14:11:46 +0000

    counter toegevoegd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-aimport random
+import random
 import re
 
 a = "Welkom bij galgje"
@@ -27,7 +27,104 @@ while True:
   if userguess == hetwoord: #als je het woord heb geraden
     print('je heb het woord ' + '"' + hetwoord + '"' + " geraden")
     break
-  
+
+  elif match: #goed geraden letter
+    print("Goed geraden")
+    for i in range(0, lengtewoord):
+      if userguess == hetwoord[i]:
+        temp = temp[:i] + userguess +temp[i+1:]
+    print(temp)
+        
+  else: # fout gereaden
+    print("nee geen match")
+    counter = counter + 1
+    if counter == 1:
+      print("""  
+     |
+     |
+     |
+     |
+     |
+_____|""")
+    elif counter == 2:
+           print("""  ____
+     |
+     |
+     |
+     |
+     |
+_____|""")
+    elif counter == 3:
+            print("""  ____
+    \|
+     |
+     |
+     |
+     |
+_____|""")
+    elif counter == 4:
+      print("""  ____
+  | \|
+     |
+     |
+     |
+     |
+_____|""")
+    elif counter == 5:
+      print("""  ____
+  | \|
+  0  |
+     |
+     |
+     |
+_____|""")
+    elif counter == 6:
+      print("""  ____
+  | \|
+  0  |
+  |  |
+     |
+     |
+_____|""")
+    elif counter == 7:
+      print("""  ____
+  | \|
+  0  |
+ /|  |
+     |
+     |
+_____|""")
+    elif counter == 8:
+      print("""  ____
+  | \|
+  0  |
+ /|\ |
+     |
+     |
+_____|""")
+    elif counter == 9:
+      print("""  ____
+  | \|
+  0  |
+ -|- |
+ /   |
+     |
+_____|""")
+    elif counter ==10:
+      print("Helaas, je bent dood")
+      print("""  ____
+  | \|
+  0  |
+ -|- |
+ / \ |
+     |
+_____|""")
+      print('het woord was ' + '"' + hetwoord + '"')
+      break
+    else:
+     pass
+
+
 
 
 
